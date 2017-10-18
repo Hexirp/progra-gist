5bb9c66d48af2b724100e8a28e19ce99643fa318
Hexirp <Hexirp@users.noreply.github.com>
2017-10-18 21:19:35 +0900

    Add Bifunctor

--- Categorical.hs
+++ Categorical.hs
@@ -8,8 +8,11 @@
 data Category :: (k -> k -> *) -> * where
  Category :: forall cat. (forall a. cat a a) -> (forall a b c. cat b c -> cat a b -> cat a c) -> Category cat
 
-data Functor :: (k -> k) -> (k -> k -> *) -> (k -> k -> *) -> * where
+data Functor :: (k0 -> k1) -> (k0 -> k0 -> *) -> (k1 -> k1 -> *) -> * where
  Functor :: forall f cat dat. Category cat -> Category dat -> (forall a b. cat a b -> dat (f a) (f b)) -> Functor f cat dat
 
 data NatTrns :: (k -> k) -> (k -> k) -> (k -> k -> *) -> (k -> k -> *) -> * where
  NatTrns :: forall f g cat dat. Category cat -> Category dat -> Functor f cat dat -> Functor g cat dat -> (forall a. dat (f a) (g a)) -> NatTrns f g cat dat
+
+data Bifunctor :: (k0 -> k1 -> k2) -> (k0 -> k0 -> *) -> (k1 -> k1 -> *) -> (k2 -> k2 -> *) -> * where
+ Bifunctor :: forall f cat dat eat. Category cat -> Category dat -> Category eat -> (forall a b c d. cat a b -> dat c d -> eat (f a c) (f b c)) -> Bifunctor f cat dat eat
