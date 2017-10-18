be3cfc49f0f7285011ffbd94115cc7a50c8d38e2
Hexirp <Hexirp@users.noreply.github.com>
2017-10-19 08:54:07 +0900

    Update Categorical.hs

--- Categorical.hs
+++ Categorical.hs
@@ -18,7 +18,7 @@ data Bifunctor :: (k0 -> k1 -> k2) -> (k0 -> k0 -> *) -> (k1 -> k1 -> *) -> (k2 
  Bifunctor :: forall f cat dat eat. Category cat -> Category dat -> Category eat -> (forall a b c d. cat a b -> dat c d -> eat (f a c) (f b c)) -> Bifunctor f cat dat eat
 
 data NatIso :: k -> k -> (k -> k -> *) -> * where
- NatIso :: forall a b cat. cat a b -> cat b a -> NatIso a b cat
+ NatIso :: forall a b cat. Category cat -> cat a b -> cat b a -> NatIso a b cat
 
 data MndCat :: k -> (k -> k -> k) -> (k -> k -> *) -> * where
  MndCat :: forall i f cat. Category cat -> Bifunctor f cat cat cat -> (forall a b c. NatIso (f a (f b c)) (f (f a b) c) cat) -> (forall a. NatIso (f i a) a cat) -> (forall a. NatIso (f a i) a cat) -> MndCat i f cat
