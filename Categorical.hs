66d8dc52dd909aa472186592c427f004317294c9
Hexirp <Hexirp@users.noreply.github.com>
2017-10-18 21:00:52 +0900

    Update Categorical.hs

--- Categorical.hs
+++ Categorical.hs
@@ -1,7 +1,12 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-9.9 --install-ghc ghci
 
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE RankNTypes, PolyKinds, GADTs #-}
 
+-- これを圏論をどのくらい追うかの基準とする。
 data Category :: (k -> k -> *) -> * where
  Category :: forall cat. (forall a. cat a a) -> (forall a b c. cat b c -> cat a b -> cat a c) -> Category cat
+
+data Functor :: (k -> k) -> (k -> k -> *) -> (k -> k -> *) -> * where
+ Functor :: forall f cat dat. (forall a b. cat a b -> dat (f a) (f b)) -> Functor f cat dat
