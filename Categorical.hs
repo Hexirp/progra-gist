5a8cdd188fd2495b24cd58513cc2c2a898f56d10
Hexirp <Hexirp@users.noreply.github.com>
2017-10-18 21:12:01 +0900

    Add NatTrns

--- Categorical.hs
+++ Categorical.hs
@@ -9,4 +9,7 @@ data Category :: (k -> k -> *) -> * where
  Category :: forall cat. (forall a. cat a a) -> (forall a b c. cat b c -> cat a b -> cat a c) -> Category cat
 
 data Functor :: (k -> k) -> (k -> k -> *) -> (k -> k -> *) -> * where
- Functor :: forall f cat dat. (forall a b. cat a b -> dat (f a) (f b)) -> Functor f cat dat
+ Functor :: forall f cat dat. Category cat -> Category dat -> (forall a b. cat a b -> dat (f a) (f b)) -> Functor f cat dat
+
+data NatTrns :: (k -> k) -> (k -> k) -> (k -> k -> *) -> (k -> k -> *) -> * where
+ NatTrns :: forall f g cat dat. Category cat -> Category dat -> Functor f cat dat -> Functor g cat dat -> (forall a. dat (f a) (g a)) -> NatTrns f g cat dat
