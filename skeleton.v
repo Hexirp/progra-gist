e0725873be491e1035f686b2aaecf1081e6f4146
Hexirp <Hexirp@users.noreply.github.com>
2017-11-12 09:33:34 +0900

    Update skeleton.hs

--- skeleton.v
+++ skeleton.v
@@ -1,33 +1,12 @@
-Definition kleisli (t : Type -> Type) : Type -> Type -> Type := fun a b => a -> t b.
+Require Import Init.
 
-Section skeleton.
- Variable composed : (Type -> Type -> Type) -> Type -> Type -> Type.
- Variable leaf : forall k a b, k a b -> composed k a b.
- Variable tree : forall k a b x, k x b -> composed k a x -> composed k a b.
- Variable match_composed
-     : forall k a b r, (k a b -> r) -> (forall x, k x b -> composed k a x -> r)
-     -> composed k a b -> r.
+Inductive skeleta (t : Type -> Type) (a : Type) : Type :=
+| returns : a -> skeleta t a
+| binds : forall x, t x -> (x -> skeleta t a) -> skeleta t a.
 
- Variable skeleta : (Type -> Type) -> Type -> Type.
- Variable returns : forall t a, a -> skeleta t a.
- Variable binds : forall t a x, composed (kleisli (skeleta t)) x a -> t x -> skeleta t a.
- Variable match_skeleta
-     : forall t a r, (a -> r) -> (forall x, composed (kleisli (skeleta t)) x a -> t x -> r)
-     -> skeleta t a -> r.
-
- Definition composed_map k a b b'
-     (k_map : forall ak bk b'k, (bk -> b'k) -> k ak bk -> k ak b'k)
-     (f : b -> b')
-     (x : composed k a b)
-     : composed k a b' :=
- match_composed _ _ _ _
-  (fun xl => leaf _ _ _ (k_map _ _ _ f xl))
-  (fun _ xl xc => tree _ _ _ _ (k_map _ _ _ f xl) xc)
- x.
-
- Definition skeleta_map t a b (f : a -> b) (x : skeleta t a) : skeleta t b :=
- match_skeleta _ _ _
-  (fun xp => returns _ _ (f xp))
-  (fun xt xc xv => _)
- x.
-End skeleton.
+Fixpoint fmap t a b (f : a -> b) (x : skeleta t a) : skeleta t b :=
+ match x with
+ | returns _ _ xr => returns _ _ (f xr)
+ | binds _ _ xt xv xf => binds _ _ xt xv (fun x => fmap _ _ _ f (xf x))
+ end
+.
