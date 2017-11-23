de6478259ce28ecc1be83573266af9ecaf8cf4e5
Hexirp <Hexirp@users.noreply.github.com>
2017-11-23 18:22:14 +0900

    Add types

--- lambda.v
+++ lambda.v
@@ -1,1 +1,12 @@
-Axiom undefined : forall A : Type, A.
+Axiom undefined : forall a : Type, a.
+
+Inductive fin : nat -> Type :=
+ | Fino : forall n, fin n
+ | Fins : forall n, fin n -> fin (S n)
+.
+
+Inductive ter : nat -> Type :=
+ | Var : forall n, fin n -> ter (S n)
+ | Abs : forall n, ter (S n) -> ter n
+ | App : forall n, ter n -> ter n -> ter n
+.
