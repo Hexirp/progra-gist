d9ac330f4c7f9f75a9ea461482431a0db7840d6a
Hexirp <Hexirp@users.noreply.github.com>
2017-12-02 13:56:02 +0900

    Use le of prelude

--- lambda.v
+++ lambda.v
@@ -11,11 +11,6 @@ Inductive ter : nat -> Type :=
 | App : forall n, ter n -> ter n -> ter n
 .
 
-Inductive le (m : nat) : nat -> Type :=
-| Leo : le m m
-| Les : forall n, le m n -> le m (S n)
-.
-
 (**
 
 (h = 0, f = 0)
@@ -42,7 +37,7 @@ Inductive le (m : nat) : nat -> Type :=
 4. 置き換える項
 
 *)
-Fixpoint beta_var m n (H : le m n) (h : fin m) (f : fin m) (x : ter n) : ter n.
+Fixpoint beta_var m n (H : m <= n) (h : fin m) (f : fin m) (x : ter n) : ter n.
 Proof.
  inversion h as [hm hH | hm hp hH]; clear h.
  - (* h = 0 *)
@@ -51,7 +46,7 @@ Proof.
    refine x.
   + (* f = n *)
    refine (
-    undefined (forall m n, le (S m) n -> fin m -> ter n) fm _ _ _
+    undefined (forall m n, S m <= n -> fin m -> ter n) fm _ _ _
    ).
    *
     rewrite -> fH.
@@ -62,7 +57,7 @@ Proof.
   inversion f as [fm fH | fm fp fH]; clear f.
   + (* f = 0 *)
    refine (
-    undefined (forall m n, le (S m) n -> fin m -> ter n) hm _ _ _
+    undefined (forall m n, S m <= n -> fin m -> ter n) hm _ _ _
    ).
    *
     rewrite -> hH.
@@ -76,12 +71,12 @@ Proof.
     --
      rewrite <- HH.
      rewrite <- fH.
-     refine (Les _ _ _).
-     refine (Leo _).
+     refine (le_S _ _ _).
+     refine (le_n _).
     --
-     refine (Les _ _ _).
+     refine (le_S _ _ _).
      refine (
-      undefined (forall m n, le (S m) n -> le m n) _ _ _
+      undefined (forall m n, S m <= n -> m <= n) _ _ _
      ).
      rewrite -> fH.
      refine Hp.
