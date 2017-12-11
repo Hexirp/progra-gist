ceb632f02eb6fd431449d3fbdbfc44c152a01399
Hexirp <Hexirp@users.noreply.github.com>
2017-12-11 19:01:35 +0900

    Update HoTT.v

--- HoTT.v
+++ HoTT.v
@@ -1,59 +1,73 @@
-Inductive paths (A : Type) (a : A) : A -> Type :=
-| idpath : paths A a a
+Local Unset Elimination Schemes.
+
+Inductive eq (A : Type) (a : A) : A -> Type :=
+| eq_refl : eq A a a
 .
 
-Definition paths_ind' A (P : forall a b, paths A a b -> Type)
-  : (forall a, P a a (idpath A a)) -> forall a b (p : paths A a b), P a b p.
+Scheme eq_ind := Induction for eq Sort Type.
+Scheme eq_rec := Minimality for eq Sort Type.
+Definition eq_rect := eq_ind.
+
+Inductive ex (A : Type) (P : A -> Type) : Type :=
+| ex_intro : forall x, P x -> ex A P
+.
+
+Definition contr (A : Type) : Type := ex A (fun x => forall y , eq A x y).
+
+Fixpoint trunc (n : nat) (A : Type) : Type :=
+ match n with 
+ | O => contr A
+ | S np => forall x y, trunc np (eq A x y)
+ end
+.
+
+Inductive Unit : Type :=
+| tt : Unit
+.
+
+Definition unit_trunc : trunc 0 Unit.
 Proof.
- intros I a b p.
- destruct p.
- apply I.
-Defined.
+ unfold trunc.
+ unfold contr.
+ refine (
+  ex_intro Unit (fun x => forall y, eq Unit x y) tt _
+ ).
+ refine (
+  fun y => _
+ ).
+ refine (
+  match y as y' return eq Unit tt y' with
+  | tt => _
+  end
+ ).
+ refine (
+  eq_refl Unit tt
+ ).
+Qed.
 
-Definition inverse A (x y : A) (p : paths A x y) : paths A y x.
+Inductive Empty : Type :=
+.
+
+Definition empty_trunc : trunc 1 Empty.
 Proof.
- destruct p.
- apply idpath.
-Defined.
+ unfold trunc.
+ refine (
+  fun x y => _
+ ).
+ refine (
+  match x as x' return contr (eq Empty x' y) with
+  end
+ ).
+Qed.
 
-Definition concat A (x y z : A) (p : paths A x y) (q : paths A y z) : paths A x z.
+Definition trunc_up : forall n A, trunc n A -> trunc (S n) A.
 Proof.
- destruct p.
- apply q.
-Defined.
-
-Definition transport (A : Type) (P : A -> Type) (x y : A) (p : x = y) (u : P x) : P y.
-Proof.
- destruct p.
- apply u.
-Defined.
-
-Definition ap A B (f : A -> B) (x y : A) (p : paths A x y) : paths B (f x) (f y).
-Proof.
- destruct p.
- apply idpath.
-Defined.
-
-Definition Sect A B (s : A -> B) (r : B -> A) : Type
-  := forall x, paths A (r (s x)) x.
-
-(* 普通の同型 *)
-Class Qinv (A B : Type) (f : A -> B) := BuildQinv {
- qinv_inv : B -> A;
- qinvl : Sect _ _ qinv_inv f;
- qinvr : Sect _ _ f qinv_inv;
-}.
-
-(* 両側可逆性 *)
-Class Isequiv (A B : Type) (f : A -> B) := BuildIsequiv {
- linv : sigT (fun g => Sect _ _ g f) ;
- rinv : sigT (fun g => Sect _ _ f g) ;
-}.
-
-(* 半随伴同値 *)
-Class IsEquiv (A B : Type) (f : A -> B) := BuildIsEquiv {
- equiv_inv : B -> A;
- eisretr : Sect _ _ equiv_inv f;
- eissect : Sect _ _ f equiv_inv;
- eisadj : forall x, paths _ (eisretr (f x)) (ap _ _ f _ _ (eissect x))
-}.
+ refine (
+  fun n A H => _
+ ).
+ unfold trunc.
+ fold (forall x y, trunc n (eq A x y)).
+ refine (
+  fun x y => _
+ ).
+ unfold trunc.
