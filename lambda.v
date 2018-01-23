292529dfd2b78484d546abc4468b22a8c658d0f7
Hexirp <Hexirp@users.noreply.github.com>
2018-01-23 21:09:24 +0900

    Update

--- lambda.v
+++ lambda.v
@@ -11,4 +11,30 @@ Inductive ter : nat -> Type :=
 | app : forall n, ter n -> ter n -> ter n
 .
 
+(** \f \x x *)
 Definition ter_0 : ter 0 := abs 0 (abs 1 (var 1 (fo 1))).
+
+(** \f \x f x *)
+Definition ter_1 : ter 0 := abs 0 (abs 1 (app 2 (var 1 (fs 0 (fo 0))) (var 1 (fo 1)))).
+
+(** \f \x f (f x) *)
+Definition ter_2 : ter 0 :=
+ abs 0 (abs 1 (app 2
+  (var 1 (fs 0 (fo 0)))
+  (app 2
+   (var 1 (fs 0 (fo 0)))
+   (var 1 (fo 1)))
+ ))
+.
+
+(** \m \f \x f (m f x) *)
+Definition ter_s : ter 0 :=
+ abs 0 (abs 1 (abs 2 (app 3
+  (var 2 (fs 1 (fo 1)))
+  (app 3
+   (app 3
+    (var 2 (fs 1 (fs 0 (fo 0))))
+    (var 2 (fs 1 (fo 1))))
+   (var 2 (fo 2)))
+ )))
+.
