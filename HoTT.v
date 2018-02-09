0e07436feb3abb05c0fd4b2161d25f8c20363572
Hexirp <Hexirp@users.noreply.github.com>
2018-02-09 22:25:21 +0900

    Add equiv

--- HoTT.v
+++ HoTT.v
@@ -29,6 +29,25 @@ Scheme nat_ind := Induction for nat Sort Type.
 Scheme nat_rec := Minimality for nat Sort Type.
 Definition nat_rect := nat_ind.
 
+(** ** 直積 *)
+Inductive and (A B : Type) : Type :=
+| prod : A -> B -> and A B
+.
+
+Scheme and_ind := Induction for and Sort Type.
+Scheme and_rec := Minimality for and Sort Type.
+Definition and_rect := and_ind.
+
+(** ** 直和 *)
+Inductive or (A B : Type) : Type :=
+| left : A -> or A B
+| right : B -> or A B
+.
+
+Scheme or_ind := Induction for or Sort Type.
+Scheme or_rec := Minimality for or Sort Type.
+Definition or_rect := or_ind.
+
 (** ** 等式 *)
 Inductive eq (A : Type) (a : A) : A -> Type :=
 | eq_refl : eq A a a
@@ -118,3 +137,25 @@ Proof.
   apply IH.
   apply H.
 Defined.
+
+(** ** 等価性 *)
+
+Definition fiber (A B : Type) (f : A -> B) (b : B) : Type :=
+ ex A (fun a => eq B (f a) b)
+.
+
+Definition equiv (A B : Type) (f : A -> B) : Type := forall b, contr (fiber A B f b).
+
+Definition equiv_hom (A B : Type) (f : A -> B) (g : B -> A) : Type :=
+ and (forall a, eq A (g (f a)) a) (forall b, eq B (f (g b)) b)
+.
+
+Definition equiv_adj (A B : Type) (f : A -> B) (g : B -> A) : Type.
+Proof.
+Admitted.
+
+Definition iso_hom (A B : Type) (f : A -> B) : Type :=
+ and
+  (ex (B -> A) (fun g => forall a, eq A (g (f a)) a))
+  (ex (B -> A) (fun h => forall b, eq B (f (h b)) b))
+.
