d24df9f0ba414c0bbe44f1c617769224697314b0
Hexirp <Hexirp@users.noreply.github.com>
2018-03-04 14:10:03 +0900

    Add comp

--- googology.v
+++ googology.v
@@ -20,8 +20,12 @@ Definition ind : forall (P : Type), P -> (P -> P) -> nat -> P :=
    end
 .
 
+Definition comp : forall (A B C : Type), (B -> C) -> (A -> B) -> A -> C :=
+ fun (A B C : Type) (f : B -> C) (g : A -> B) (x : A) => f (g x)
+.
+
 Definition f01 : nat -> nat := ind nat O S.
 
-Definition f010 : nat -> nat := fun (x : nat) => S (f01 x).
+Definition f010 : nat -> nat := comp nat nat nat S f01.
 
-Definition f0100 : nat -> nat := fun (x : nat) => S (f010 x).
+Definition f0100 : nat -> nat := comp nat nat nat S f010.
