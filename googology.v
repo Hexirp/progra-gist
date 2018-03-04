91efea351f1e1d9155b9c828a703f5bdf54e8ebe
Hexirp <Hexirp@users.noreply.github.com>
2018-03-04 13:56:52 +0900

    Add ind

--- googology.v
+++ googology.v
@@ -10,3 +10,12 @@ Definition f : nat := O.
 Definition f0 : nat := S f.
 
 Definition f00 : nat := S f0.
+
+Definition ind : forall (P : Type), P -> (P -> P) -> nat -> P :=
+ fun (P : Type) (cO : P) (cS : P -> P) =>
+  fix go (x : nat) : P :=
+   match x return P with
+   | O => cO
+   | S xp => cS (go xp)
+   end
+.
