50cf28fa80bcd38a90d0fb36ad9724f98ecf78d9
Hexirp <Hexirp@users.noreply.github.com>
2018-03-20 21:35:52 +0900

    Add ord.v

---
+++ ord.v
@@ -0,0 +1,17 @@
+Require Import Coq.Init.Prelude.
+
+Axiom ord : Type.
+Axiom lt : ord -> ord -> Prop.
+
+Axiom ind : forall p : ord -> Prop, (forall a, (forall x, lt x a -> p x) -> p a) -> forall a, p a.
+
+Definition not_lt_refl : forall a, ~ lt a a.
+Proof.
+ apply (ind (fun a => ~ lt a a)).
+ intros a IH H.
+ apply IH with a.
+ -
+  apply H.
+ -
+  apply H.
+Qed.
