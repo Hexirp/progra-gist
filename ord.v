e8c8562f9ff1c9770de8653d7aab17f18ebc9590
Hexirp <Hexirp@users.noreply.github.com>
2018-03-21 18:17:56 +0900

    Add not_lt_sym and not_lt_inf_dec_chain

--- ord.v
+++ ord.v
@@ -7,7 +7,7 @@ Axiom ind : forall p : ord -> Prop, (forall a, (forall x, lt x a -> p x) -> p a)
 
 Definition not_lt_refl : forall a, ~ lt a a.
 Proof.
- apply (ind _).
+ apply (ind (fun a => ~ lt a a)).
  intros a IH H.
  apply IH with a.
  -
@@ -15,3 +15,40 @@ Proof.
  -
   apply H.
 Qed.
+
+Definition not_lt_sym : forall a b, ~ (lt a b /\ lt b a).
+Proof.
+ apply (ind (fun a => forall b, ~ (lt a b /\ lt b a))).
+ intros a IHa.
+ apply (ind (fun b => ~ (lt a b /\ lt b a))).
+ intros b IHb [Hl Hr].
+ apply IHa with b a.
+ -
+  apply Hr.
+ -
+  split.
+  +
+   apply Hr.
+  +
+   apply Hl.
+Qed.
+
+Section Not_lt_inf_dec_chain.
+ Variable f : nat -> ord.
+ Variable inf_dec_chain : forall n, lt (f (S n)) (f n).
+
+ Definition not_lt_inf_dec_chain : False.
+ Proof.
+  apply (ind (fun a => forall n, a = f n -> False)) with (f O) O.
+  -
+   intros a IH n H.
+   apply IH with (f (S n)) (S n).
+   +
+    rewrite H.
+    apply inf_dec_chain.
+   +
+    apply eq_refl.
+  -
+   apply eq_refl.
+ Qed.
+End Not_lt_inf_dec_chain.
