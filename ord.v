c012ab3465e68af11ee9e0a21115b4d29aedfaea
Hexirp <Hexirp@users.noreply.github.com>
2018-03-28 18:08:00 +0900

    Add Notations

--- ord.v
+++ ord.v
@@ -4,9 +4,33 @@ Declare ML Module "ltac_plugin".
 
 Global Set Default Proof Mode "Classic".
 
-Definition id : forall (A : Type) (_ : A), A.
-Proof.
- refine (fun (A : Type) (a : A) => (a : A)).
+Module Notations.
+ Reserved Notation "x -> y" (at level 99, right associativity, y at level 200).
+ Reserved Notation "x <-> y" (at level 95, no associativity).
+ Reserved Notation "x /\ y" (at level 80, right associativity).
+ Reserved Notation "x \/ y" (at level 85, right associativity).
+ Reserved Notation "~ x" (at level 75, right associativity).
+
+ Reserved Notation "x = y  :>  T" (at level 70, y at next level, no associativity).
+ Reserved Notation "x = y" (at level 70, no associativity).
+
+ Reserved Notation "x <> y  :>  T"(at level 70, y at next level, no associativity).
+ Reserved Notation "x <> y" (at level 70, no associativity).
+
+ Reserved Notation "x <= y" (at level 70, no associativity).
+ Reserved Notation "x < y" (at level 70, no associativity).
+ Reserved Notation "x >= y" (at level 70, no associativity).
+ Reserved Notation "x > y" (at level 70, no associativity).
+
+ Reserved Notation "x + y" (at level 50, left associativity).
+ Reserved Notation "x - y" (at level 50, left associativity).
+ Reserved Notation "x * y" (at level 40, left associativity).
+ Reserved Notation "x / y" (at level 40, left associativity).
+ Reserved Notation "x ^ y" (at level 30, right associativity).
+
+ Reserved Notation "- x" (at level 35, right associativity).
+ Reserved Notation "/ x" (at level 35, right associativity).
+End Notations.
 
 Definition not_and_then : forall A B : Prop, (A -> ~ B) -> ~ (A /\ B).
 Proof.
