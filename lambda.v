750cb04f4c38c1f2eb434c4fda66082dfe13de7e
Hexirp <Hexirp@users.noreply.github.com>
2018-05-16 19:40:16 +0900

    Update

--- lambda.v
+++ lambda.v
@@ -77,45 +77,13 @@ Proof.
   apply eq_refl.
 Defined.
 
-Definition loose_gen_beta_red_var : forall m n, fin (m + S n) -> lam (m + n) -> lam (m + n).
-Proof.
- fix go 3.
- intros m n v y.
- refine (
-  match v in fin n' return n' = m + S n -> lam (m + n) with | fo N => _ | fs N vp => _ end
-  eq_refl
- ).
- -
-  intros vH.
-  refine (
-   match n as n' return n' = n -> lam (m + n) with | O => _ | S np => _ end
-   eq_refl
-  ).
-  +
-   intros nH.
-   apply y.
-  +
-   intros nH.
-   case nH.
-   apply var.
-   case plus_1_mn with m np.
-   apply fo.
- -
-  intros vH.
-  refine (
-   match n as n' return n' = n -> lam (m + n) with | O => _ | S np => _ end
-   eq_refl
-  ).
-  +
-   intros nH.
-   
-
 Definition loose_gen_beta_red_var_by_ind
     : forall N, fin N -> forall m n, m + S n = N -> lam (m + n) -> lam (m + n).
 Proof.
  refine (ind_fin _ _ _).
  -
-  intros N m [ | np ] p y.
+  intros N m n p y.
+  destruct n as [ | np ].
   +
    apply y.
   +
@@ -123,9 +91,12 @@ Proof.
    case plus_1_mn with m np.
    apply fo.
  -
-  intros N vp H [ | mp ] n p y.
+  intros N vp H m n p y.
+  destruct n as [ | np ].
   +
-   
+   destruct m as [ | mp ].
+   *
+    exfalso.
 
 Definition loose_gen_beta_red_by_ind
     : forall N, lam N -> forall m n, m + S n = N -> lam (m + n) -> lam (m + n).
