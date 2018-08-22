d7157d2d26fd50f89fb03ddeb5647d31ba950161
Hexirp <Hexirp@users.noreply.github.com>
2018-08-22 22:17:32 +0900

    Update: bool を追加しており下げ

--- glow.v
+++ glow.v
@@ -1,3 +1,19 @@
 (* in Coq 8.8.0 -no-is *)
 
-Notation "x -> y" := (forall (_ : x), y) (at level 99, right associativity, y at level 200).
+Notation "x -> y" := (forall (_ : x), y)
+ (at level 99, right associativity, y at level 200)
+.
+
+Inductive bool : Type :=
+| false : bool
+| true : bool
+.
+
+Definition bool_rec
+ {P : Type} (cf : P) (ct : P) (x : bool) : P
+:=
+ match x with
+ | false => cf
+ | true => ct
+ end
+.
